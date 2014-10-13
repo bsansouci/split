3cd108392fb6c5daa337b46a247101a163eb7147
Benjamin San Souci <benjamin.sansouci@gmail.com>
2014-10-12 21:27:43 -0400

    added request

--- server.js
+++ server.js
@@ -24,24 +24,13 @@ secure.use(bodyParser.urlencoded());
 secure.post("/", function(req, res, next){
   res.sendFile("index.html", { root: path.join(__dirname, 'client') });
 });
-//app.get("*", function (req, res, next) {
-//  res.redirect("https://" + req.headers.host + "/" + (req.path === "/" ? "" : req.path));
-//});
-
-//app.post("*", function (req, res, next) {
-//  res.redirect("https://" + req.headers.host + "/" + (req.path === "/" ? "" : req.path));
-//});
 
 var privateKey  = fs.readFileSync(__dirname + '/ssl/server.key', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/ssl/server.crt', 'utf8');
 
 var credentials = {
   key: privateKey,
-  cert: certificate,
-  // ca: [
-  //   fs.readFileSync(__dirname + '/ssl/cert2.pem', 'utf8'),
-  //   fs.readFileSync(__dirname + '/ssl/cert3.pem', 'utf8')
-  // ]
+  cert: certificate
 };
 
 https.createServer(credentials, secure).listen(443, function() {
