a484ddd1b0116fae47d2893bc28aa0ab396ef053
Benjamin San Souci <benjamin.sansouci@gmail.com>
2014-10-18 01:36:07 -0400

    added config.js

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ var https = require('https');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require("path");
+
+var config = require("./config");
+
 var app = express();
 
 app.use(express.static(__dirname + '/client/'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
-http.createServer(app).listen(80, function() {
-  console.log("HTTP server running on port", 80);
+http.createServer(app).listen(config.port, function() {
+  console.log("HTTP server running on port", config.port);
 });
 
