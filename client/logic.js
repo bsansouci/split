c95a2e63be2888e542ffe1c2917ed94cfa5ee997
Schmavery <elflord@live.ca>
2014-10-18 01:41:39 -0400

    Fix initialization issues

--- client/logic.js
+++ client/logic.js
@@ -2,28 +2,20 @@ var BOARD_SIZE = 8;
 var NUM_ROWS = 3;
 
 function initialize(){
-  for (var i = 0; i < BOARD_SIZE; i++){
+  for (var i = 0; i < BOARD_SIZE/2; i++){
     for (var j = 0; j < NUM_ROWS; j++){
       var enemy = new Piece();
       enemy.ally = false;
-      enemy.x = ((i+1)%2) + ~~(i/2);
-      enemy.y = i;
+      enemy.x = ((j+1)%2) + ~~(i*2);
+      enemy.y = j;
       board[enemy.x][enemy.y] = enemy;
 
       var ally = new Piece();
-      ally.x = ((i)%2) + ~~(i/2);
-      ally.y = BOARD_SIZE - 1 - i;
+      ally.x = (j%2) + ~~(i*2);
+      ally.y = BOARD_SIZE - 1 - j;
       board[ally.x][ally.y] = ally;
     }
   }
-}
-
-function makeMove(move) {
-
-}
-
-function makeEnemyMove() {
-
 }
 
 function isValid(x, y) {
