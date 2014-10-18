de79ea2eb9353a7f244424cde3dca88f319742b5
Schmavery <elflord@live.ca>
2014-10-18 00:19:47 -0400

    Initialize board with pieces

---
+++ client/logic.js
@@ -0,0 +1,32 @@
+var board;
+var BOARD_SIZE = 8;
+var NUM_ROWS = 3;
+
+function initialize(){
+  for (int i = 0; i < BOARD_SIZE; i++){
+    for (int j = 0; j < NUM_ROWS; j++){
+      var enemy = new Piece();
+      enemy.ally = false;
+      enemy.x = ((i+1)%2) + ~~(i/2);
+      enemy.y = i;
+      board[enemy.x][enemy.y] = enemy;
+
+      var ally = new Piece();
+      ally.x = ((i)%2) + ~~(i/2);
+      ally.y = BOARD_SIZE - 1 - i;
+      board[ally.x][ally.y] = ally;
+    }
+  }
+}
+
+function makeMove(move) {
+  
+}
+
+function makeEnemyMove() {
+  
+}
+
+function possibleMoves() {
+  
+}
